4e434085710d375fd38e7da2dab5ddda1a7a4ca4
MuhammadYounas124 <younasraja300@gmail.com>
2024-11-06 18:05:06 +0300

    new file add

--- Advanced-login-project/src/App.tsx
+++ Advanced-login-project/src/App.tsx
@@ -2,16 +2,7 @@ const App = () => {
   return (
     <div className="login-container">
       <h2 className="form-title">Log in with</h2>
-      <div className="social-login">
-        <button className="social-button">
-        <img src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo" className="social-icon"/>
-        Google
-        </button>
-        <button className="social-button">
-        <img src="https://img.icons8.com/ios-filled/50/mac-os.png" alt="mac-os" className="social-icon"/>
-        Apple
-        </button>
-      </div>
+      <socialLogin/>
       <p className="seperator"><span>or</span></p>
 
       <form action="#" className="login-form">
